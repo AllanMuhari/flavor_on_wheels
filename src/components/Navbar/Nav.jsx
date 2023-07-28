011854669c4bf51c457fcee37dc9119f643cf02e
AllanMuhari <allanmuhari087@gmail.com>
2023-07-28 17:31:54 +0300

    nav

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Nav = () => {
+    const navData = [
+        {name:"Home"},
+        {name:"Services"},
+        {name:"Dishes"},
+        {name:"Contact"}
+       ];
   return (
     <div>Nav</div>
   )
