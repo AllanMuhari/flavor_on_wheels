0afce1957be90dad05b5cb04fc40f0caa5a13c04
AllanMuhari <allanmuhari087@gmail.com>
2023-07-31 15:39:46 +0300

    app.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from "./components/Navbar/Nav.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+     
+    </div>
+  );
+}
+
+export default App;
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 
 const Nav = () => {
-    const navData = [
-        {name:"Home"},
-        {name:"Services"},
-        {name:"Dishes"},
-        {name:"Contact"}
-       ];
   return (
     <div>
-        
+        HELLO
     </div>
   )
 }
