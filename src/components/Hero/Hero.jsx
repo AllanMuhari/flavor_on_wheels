095c49223dff2e544d6c9a1846799ecb91ea393d
AllanMuhari <allanmuhari087@gmail.com>
2023-08-12 02:27:13 +0300

    Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,16 +8,17 @@ const Hero = () => {
       <div className='max-h-[500px] relative'>
         {/* overlay */}
         <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
-          <h1 className='px-4 text-4xl sm:text-5xl md:text:6xl lg:text-7xl font-bold '>
+          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
             The <span className='text-orange-500'>Best</span>
           </h1>
-          <h1 className='px-4 text-4xl sm:text-5xl md:text:6xl lg:text-7xl font-bold '>
-            Foods <span className='text-orange-500'>Delivered</span>
+          <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
+            {" "}
+            <span className='text-orange-500'>Foods</span> Devlivered
           </h1>
         </div>
         <img
-          className='w-full max-h-[500px] object-fill'
-          src='https://images.pexels.com/photos/1639565/pexels-photo-1639565.jpeg?auto=compress&cs=tinysrgb&w=600'
+          className='w-full max-h-[500px] object-cover'
+          src='https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
           alt=''
         ></img>
       </div>
