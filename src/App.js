c8c4b298be84ecc5caceeb75cf6f3af803e2dddd
AllanMuhari <allanmuhari087@gmail.com>
2023-08-12 02:28:42 +0300

    App js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
 import Navbar from "./components/Navbar/Nav.jsx";
 import Hero from "./components/Hero/Hero.jsx";
+import Cards from "./components/Cards/Cards.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Cards/>
     </div>
   );
 }
