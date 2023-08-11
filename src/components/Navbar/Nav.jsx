e095caa713ad6d2ddf1c2dcd6be1b683c48549df
AllanMuhari <allanmuhari087@gmail.com>
2023-08-12 01:36:58 +0300

    state navbar

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import {
   AiOutlineMenu,
   AiOutlineSearch,
@@ -13,15 +13,17 @@ import { FaUserFriends, FaWallet } from "react-icons/fa";
 import { MdFavorite, MdHelp } from "react-icons/md";
 
 const Nav = () => {
+  const [nav, setNav] = useState(false);
   return (
     <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+      {/* left */}
       <div className='flex items-center'>
-        <div className='cursor-pointer'>
+        <div onClick={() => setNav(!nav)} className='cursor-pointer'>
           <AiOutlineMenu size={30} />
         </div>
         <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
-          Best
-          <span className='font-bold'>Eat</span>
+          Flavor
+          <span className='font-bold'>On Wheels</span>
         </h1>
         <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
           <p className='bg-black text-white rounded-full p-2'>Delivery</p>
@@ -41,15 +43,28 @@ const Nav = () => {
         <BsFillCartFill size={20} className='mr-2' /> Cart
       </button>
       {/* mobile menu */}
-      <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+
+      {nav ? (
+        <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+      ) : (
+        ""
+      )}
+
       {/* side drawer */}
-      <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
+      <div
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
+      >
         <AiOutlineClose
+          onClick={() => setNav(!nav)}
           size={30}
           className='absolute right-4 top-4 cursor-pointer'
         />
         <h2 className='text-2xl p-4'>
-          Best <span className='font-bold'>Eats</span>
+          Flavor <span className='font-bold'>On Wheels</span>
         </h2>
         <nav>
           <ul className='flex flex-col p-4 text-gray-800'>
